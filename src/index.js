4ab2f7e41af8375ba478d69055dc6db3d949fd08
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-09-25 13:52:42 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from "react"; // Importa a biblioteca React
+import ReactDOM from "react-dom"; // Importa o ReactDOM para manipulação do DOM
+import App from "./App"; // Importa o componente App que contém a lógica da aplicação
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> {/* Habilita o modo estrito para ajudar no desenvolvimento */}
+    <App /> {/* Renderiza o componente App */}
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root") // O componente App é montado no elemento com id "root" no HTML
 );
